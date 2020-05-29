7e0df0309cad691cba215cec4de715b659bfe8e6
GatroxM <gamunoz@distry.co>
2020-05-29 16:28:27 -0500

    Cambio inicio sección 8

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [
+  ]
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() {
+    
+    this.contarTres().then(
+      mensaje => console.log('Termino!', mensaje)
+    ).catch( error => console.error('Error en la promesa ', error));
+  }
+
+  ngOnInit(): void {
+  }
+
+  contarTres(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+
+      let cont = 0;
+      const intervalo = setInterval( () => {
+
+        cont += 1;
+        console.log(cont);
+        if (cont === 3 ) {
+          resolve(true);
+          clearInterval( intervalo );
+        }
+
+      }, 1000);
+
+    });
+  }
+
+}
